f1f2030524077cb6049167c3a6508be50a4f339c
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-05-26 23:02:27 -0400

    pushing to hub for submission

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+from surveys import *
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "!1IcedCoffee"
+debug = DebugToolbarExtension(app) 
+
+responses = []
+button_content = 'start'
+
+@app.route('/')
+def home():
+    responses.clear()
+    surv_title = satisfaction_survey.title
+    surv_instructions = satisfaction_survey.instructions
+    return render_template('base.html', title = surv_title, instructions = surv_instructions, current = 0, button = button_content, method = "get")
+
+@app.route('/<int:current>', methods=["GET", "POST"])
+def question(current):
+    if len(request.form) == 0 and current > 0:
+        flash('must submit questions in order!')
+        return redirect('/')
+    if len(request.form) > 0:
+        responses.append(request.form["answer"])
+    if current <= 3:
+        frm_method = "post"
+        current_q = current + 1
+        button_content = 'next'
+        return render_template('question.html', question = satisfaction_survey.questions[current].question, choices = satisfaction_survey.questions[current].choices, button = button_content, current = current_q, method = frm_method)
+    return render_template('thanks.html', button = "return to start", current = '/', answers = responses)
