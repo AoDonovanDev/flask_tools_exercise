f4f43e9b7c6d75311c9c6901761b3da4db7b8753
Andrew Donovan <a.o.donovan.music@gmail.com>
2023-05-27 01:28:18 -0400

    implement flask session in place of list variable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import *
@@ -8,12 +8,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "!1IcedCoffee"
 debug = DebugToolbarExtension(app) 
 
-responses = []
 button_content = 'start'
 
 @app.route('/')
 def home():
-    responses.clear()
+    session['responses'] = []
     surv_title = satisfaction_survey.title
     surv_instructions = satisfaction_survey.instructions
     return render_template('base.html', title = surv_title, instructions = surv_instructions, current = 0, button = button_content, method = "get")
@@ -24,10 +23,12 @@ def question(current):
         flash('must submit questions in order!')
         return redirect('/')
     if len(request.form) > 0:
-        responses.append(request.form["answer"])
+        answers = session['responses']
+        answers.append(request.form["answer"])
+        session['responses'] = answers
     if current <= 3:
         frm_method = "post"
-        current_q = current + 1
+        next = current + 1
         button_content = 'next'
-        return render_template('question.html', question = satisfaction_survey.questions[current].question, choices = satisfaction_survey.questions[current].choices, button = button_content, current = current_q, method = frm_method)
-    return render_template('thanks.html', button = "return to start", current = '/', answers = responses)
+        return render_template('question.html', question = satisfaction_survey.questions[current].question, choices = satisfaction_survey.questions[current].choices, button = button_content, current = next, method = frm_method, number = next)
+    return render_template('thanks.html', button = "return to start", current = '/', answers = session['responses'])
